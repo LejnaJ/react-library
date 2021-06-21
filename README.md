bbdd3d7915b933e8acf326d4ca69fc01343c8aa8
LejnaJ <lejna@ito.ba>
2021-06-21 21:07:14 +0200

    basic setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-library
 Book search with React.js and Google Books API
+
+A WIP, would love to add some more features.
--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
+import './App.scss';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import mainImg from './assets/books.jpg';
-import logo from './assets/logo.png';
+
 function App() {
 
   const [result, setResult] = useState([]);
@@ -32,52 +32,60 @@ function App() {
     <header className="header-app">
       <div class="container">
       <div className="logo-wrapper">
-      <img src={logo} />
+      <div>Book<span>Finder</span></div>
       </div>
-      <p>
-      Book finder
-      </p>
+      <ul class="main-nav">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Contact</a></li>
+      </ul>
       </div>
     </header>
     <div className="container">
     <div className="content main d-flex align-items-center justify-content-center">
+ 
           <div className="text">
-              <div className="introduction-title"><span>Lorem Ipsum</span></div>
-              <div className="introduction-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Curabitur sit amet odio et ligula volutpat eleifend.
-                  Donec ex magna, rutrum et sagittis quis, volutpat in nunc.</div>
+              <div className="introduction-title"><span>BookFinder</span></div>
+              <div className="introduction-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut efficitur sem nec imperdiet volutpat. Morbi rutrum sollicitudin aliquam. Nam ullamcorper nibh sed sapien semper, vel efficitur justo rutrum. Morbi ultricies libero quis dapibus posuere.  </div>
+              <div><a href="#section-search" class="btn btn-primary">Search</a></div>
           </div>
           <div className="book-img ">
               <img src={mainImg} />
-          </div>
-       
-      
+          </div>  
       </div>
-  
-      <div className="form-wrapper">
+      {/* I need to do something about this section.. */}
+      <div id="section-search">
+        <div class="search-heading d-flex flex-row">
+      <div className="form-wrapper mb-4">
             <form onSubmit={handleSubmit} className="d-flex flex-row">
               <div className="form-group">
                 <input type="text" className="form-control" placeholder="Search" autoComplete="off" onChange={handleChange}/>
               </div>
-              <button type="submit" className="btn btn-danger">Search</button>
+              {/* Should eventually switch to icons.. */}
+              <button type="submit" className="btn btn-primary">S</button>
             </form>
+          </div>
+          <div class="show-as d-flex">
+           <p class="active">List</p>
+           <p>Cards</p>
+          </div>
           </div>
           <div className="row results">
             {result.map(book => (
               <div className="col-3 book-item">
-              <a href={book.volumeInfo.previewLink} target="_blank" >
+              <a href={book.volumeInfo.previewLink} target="_blank"  rel="noreferrer" >
               <img  src={book.volumeInfo.imageLinks === undefined
                 ? ""
                 : `${book.volumeInfo.imageLinks.thumbnail}`} alt={book.title} />
                 </a>
                 <p>{book.volumeInfo.title}</p>
-                <p>{book.volumeInfo.authors}</p>
-                {/* <p>{book.volumeInfo.description}</p> */}
+              
                 </div>
             ))}
           </div>
   </div>
-
+  </div>
   </div>
   );
 }
